f3a687bdcbaf339b1e34ff7944eb3adc4bd681db
Aleksey Efimov <Aleks4404@mail.ru>
2021-07-01 08:33:24 +0400

    A project has been created HomeRage

--- src/main/java/com/java24hours/HomePage.java
+++ src/main/java/com/java24hours/HomePage.java
@@ -3,19 +3,19 @@ package com.java24hours;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-public class HomeRage {
+public class HomePage {
     String owner;
     URL address;
     String category = "нет";
 
-    public HomeRage(String inOwner, String inAddress)
+    public HomePage(String inOwner, String inAddress)
             throws MalformedURLException {
 
         owner = inOwner;
         address = new URL(inAddress);
     }
 
-    public HomeRage(String inOwner, String inAddress,
+    public HomePage(String inOwner, String inAddress,
                     String inCategory)
             throws MalformedURLException {
 
